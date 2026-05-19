d4dd84019266ec32a4bd9495980ddd78fc55ee56
nazumods <nazumods@gmail.com>
2026-05-19 16:03:59 -0400

    style: /wbc stat styling

--- Warbandeer_Characters/dump.lua
+++ Warbandeer_Characters/dump.lua
@@ -1,13 +1,20 @@
 local _, ns = ...
 local print = print
+local GetBuildInfo = GetBuildInfo -- luacheck: globals GetBuildInfo
+
+local TIME_LONG = "%d days %d hours %d minutes"
+local TIME_SHORT = "%dd %dh %dm"
 
 ns:registerCommand("stat", "", function(self)
-  local a, h = 0, 0
+  local patch = select(1, GetBuildInfo())
+  local a, h, p, pp = 0, 0, 0, 0
   local mp, ft = 0, nil
   local pbc = {}
   for _,t in pairs(self.db.characters) do
     if t.isAlliance then a = a + 1 else h = h + 1 end
     if t.playtime then
+      p = p + t.playtime.total
+      pp = pp + t.playtime.byPatch[patch]
       if t.playtime.total > mp then
         mp = t.playtime.total
         ft = t.name
@@ -24,12 +31,13 @@ ns:registerCommand("stat", "", function(self)
   end
 
   self.Print("Warband Statistics:")
-  print(self.db.numCharacters .. " characters, " .. a .. " alliance, " .. h .. " horde")
+  print(self.db.numCharacters .. " characters, " .. a .. " alliance, " .. h .. " horde. " .. roundTime(p) .. " played, " .. formatTime(pp, TIME_SHORT) .. " this patch.")
   print("Most played: " .. ft .. " (" .. formatTime(mp) .. ")")
   print("Most played class: " .. cn .. " (" .. formatTime(cp) .. ")")
 end, "Dump current character data")
 
-function formatTime(seconds)
+function formatTime(seconds, fmt)
+  fmt = fmt or TIME_LONG
   local days = 0
   local hours = math.floor(seconds / 3600)
   local minutes = math.floor((seconds % 3600) / 60)
@@ -37,5 +45,9 @@ function formatTime(seconds)
     days = math.floor(hours / 24)
     hours = hours % 24
   end
-  return string.format("%d days %d hours %d minutes", days, hours, minutes)
+  return string.format(fmt, days, hours, minutes)
+end
+
+function roundTime(seconds)
+  return string.format("%.2f days", seconds / 86400)
 end
